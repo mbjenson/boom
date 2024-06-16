8035291766626ee104400ea54f40c2e0a474e4b0
Theodlz <theophile.dulaz@gmail.com>
2024-06-16 09:42:56 -0700

    split consume and consume_with_max, test both

--- src/bin/producer.rs
+++ src/bin/producer.rs
@@ -7,13 +7,13 @@ async fn main() {
     let uri = rabbitmq::get_uri();
     let connection = rabbitmq::connect(&uri).await;
     let channel = rabbitmq::create_channel(&connection).await;
-    rabbitmq::declare_queue(&channel, "queue_test").await;
+    rabbitmq::declare_queue(&channel, "ztf_alerts").await;
 
     let files = std::fs::read_dir("data/ztf_alerts").unwrap();
     for (i, f) in files.enumerate() {
         let f = f.unwrap();
         let msg = std::fs::read(f.path()).unwrap();
-        rabbitmq::publish_message(&channel, "", "queue_test", &msg).await;
+        rabbitmq::publish_message(&channel, "", "ztf_alerts", &msg).await;
         println!("Sent message: {}", i);
     }
 }
