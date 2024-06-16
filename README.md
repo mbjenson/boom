985766153694bcf02d818aa207c9b5dc1d7f3b52
Theodlz <theophile.dulaz@gmail.com>
2024-06-15 20:25:19 -0700

    migrate from amqprs to lapin

--- README.md
+++ README.md
@@ -9,7 +9,7 @@
 #### Run it:
 1. First, add the rabbitmq_management plugin with `sudo rabbitmq-plugins enable rabbitmq_management`. Then head to the [dashboard's exchanges section](http://localhost:15672/#/exchanges), and add an exchange called `ztf_alerts`.
 2. In a terminal, run `cargo build`
-3. In another terminal, run `cargo run --bin publisher`. This will start pushing the demo alert (from `data/alert.avro`) to a work queue.
+3. In another terminal, run `cargo run --bin publisher`. This will start pushing the demo alert (from `data/alerts/alert.avro`) to a work queue.
 4. In yet another terminal, run `cargo run --bin consumer`. This will spawn one consumer that will read from the queue and print the candid + objectId of the alert.
 You should see that the first alert is processed successfully, and thereafter the consumer should tell you that the alert already exists in the database.
 
--- src/bin/consumer.rs
+++ src/bin/consumer.rs
@@ -1,4 +1,9 @@
-use amqprs::{channel::QueueBindArguments, connection::Connection, FieldTable};
+use lapin::{
+    message::DeliveryResult,
+    options::{BasicAckOptions, BasicConsumeOptions, QueueDeclareOptions},
+    types::FieldTable,
+    Connection, ConnectionProperties,
+};
 use apache_avro::from_value;
 use apache_avro::Reader;
 use std::io::BufReader;
@@ -6,74 +11,6 @@ use std::io::BufReader;
 mod structs;
 mod database;
 mod utils;
-
-struct RabbitConnect {
-    host: String,
-    port: u16,
-    username: String,
-    password: String,
-}
-
-async fn connect_rabbitmq(
-    connection_details: &RabbitConnect,
-) -> Connection {
-    let connection = Connection::open(&amqprs::connection::OpenConnectionArguments::new(
-        &connection_details.host,
-        connection_details.port,
-        &connection_details.username,
-        &connection_details.password,
-    ).virtual_host("/"))
-    .await
-    .unwrap();
-    connection.register_callback(amqprs::callbacks::DefaultConnectionCallback)
-        .await
-        .unwrap();
-    connection
-}
-
-async fn channel_rabbitmq(connection: &mut Connection) -> amqprs::channel::Channel {
-    let channel = connection.open_channel(None).await.unwrap();
-    channel.register_callback(amqprs::callbacks::DefaultChannelCallback)
-        .await
-        .unwrap();
-    channel
-}
-
-async fn bind_queue_to_exchange(
-    connection: &mut Connection,
-    channel: &mut amqprs::channel::Channel,
-    connection_details: &RabbitConnect,
-    queue: &str,
-) {
-    if !connection.is_open() {
-        println!("Connection is closed. Reconnecting...");
-        *connection = connect_rabbitmq(connection_details).await;
-        *channel = channel_rabbitmq(connection).await;
-        println!("Reconnected: {}", connection);
-    }
-
-    let args = FieldTable::default();
-
-    let qparams = amqprs::channel::QueueDeclareArguments::default()
-        .queue(queue.to_owned())
-        .auto_delete(false)
-        .durable(true)
-        .arguments(args)
-        .finish();
-
-    let (queue, _, _) = channel.queue_declare(qparams).await.unwrap().unwrap();
-
-    if !channel.is_open() {
-        println!("Channel is closed. Reopening...");
-        *channel = channel_rabbitmq(connection).await;
-        println!("Reopened: {}", channel);
-    }
-
-    // bind the queue to the exchange
-    channel.queue_bind(
-        QueueBindArguments::new(&queue, "ztf_alerts", "")
-    ).await.unwrap();
-}
 
 async fn process_record(record: apache_avro::types::Value) {
     let alert_packet: structs::AlertPacket = from_value(&record).unwrap();
@@ -113,76 +50,75 @@ async fn process_record(record: apache_avro::types::Value) {
         database::save_alert_aux(alert_aux).await;
     }
 
+    // simulate processing time
+    // tokio::time::sleep(tokio::time::Duration::from_millis(1000)).await;
+
     // we are done processing the alert
-    println!("Processed alert: {}", alert_packet.candid);
+    // println!("Processed alert: {}", alert_packet.candid);
 }
-    
-
-async fn receive(
-    connection: &mut Connection,
-    channel: &mut amqprs::channel::Channel,
-    connection_details: &RabbitConnect,
-) {
-    if !connection.is_open() {
-        println!("Connection is closed. Reconnecting...");
-        *connection = connect_rabbitmq(connection_details).await;
-        *channel = channel_rabbitmq(connection).await;
-        println!("Reconnected: {}", connection);
-    }
-
-    if !channel.is_open() {
-        println!("Channel is closed. Reopening...");
-        *channel = channel_rabbitmq(connection).await;
-        println!("Reopened: {}", channel);
-    }
-
-    let queue = "ztf_alerts_workers";
-    let args = amqprs::channel::BasicConsumeArguments::new(
-        queue,
-        "",
-    );
-    bind_queue_to_exchange(connection, channel, connection_details, queue).await;
-
-    let (_ctag, mut message_rx) = channel.basic_consume_rx(args.clone()).await.unwrap();
-    println!("Consuming messages from queue: ztf_alerts");
-
-    let mut i = 0;
-    while let Some(message) = message_rx.recv().await {
-        let content = message.content.unwrap();
-        // content is a Vec<u8>, representing the content of an avro file
-        let reader = Reader::new(BufReader::new(&content[..])).unwrap();
-        for record in reader {
-            let record = record.unwrap();
-            let alert_packet: structs::AlertPacket = from_value(&record).unwrap();
-            // print the objectId and candid of the alert
-            println!("{}: objectId: {}, candid: {}", i, alert_packet.objectId, alert_packet.candid);
-
-            // we can now process the alert
-            process_record(record).await;
-        }
-        let args = amqprs::channel::BasicAckArguments::new(message.deliver.unwrap().delivery_tag(), false);
-        // acknowledge the message
-        channel.basic_ack(args).await.unwrap();
-        i += 1;
-    }
-}
-
 
 #[tokio::main]
 async fn main() {
-    let connection_details = RabbitConnect {
-        host: "localhost".to_string(),
-        port: 5672,
-        username: "guest".to_string(),
-        password: "guest".to_string(),
-    };
+    let uri = "amqp://localhost:5672";
+    let options = ConnectionProperties::default()
+        // Use tokio executor and reactor.
+        // At the moment the reactor is only available for unix.
+        .with_executor(tokio_executor_trait::Tokio::current())
+        .with_reactor(tokio_reactor_trait::Tokio);
 
-    let mut connection = connect_rabbitmq(&connection_details).await;
-    let mut channel = channel_rabbitmq(&mut connection).await;
+    let connection = Connection::connect(uri, options).await.unwrap();
+    let channel = connection.create_channel().await.unwrap();
 
-    receive(
-        &mut connection,
-        &mut channel,
-        &connection_details,
-    ).await;
+    let _queue = channel
+        .queue_declare(
+            "queue_test",
+            QueueDeclareOptions::default(),
+            FieldTable::default(),
+        )
+        .await
+        .unwrap();
+
+    let consumer = channel
+        .basic_consume(
+            "queue_test",
+            "tag_foo",
+            BasicConsumeOptions::default(),
+            FieldTable::default(),
+        )
+        .await
+        .unwrap();
+
+    consumer.set_delegate(move |delivery: DeliveryResult| async move {
+        let delivery = match delivery {
+            // Carries the delivery alongside its channel
+            Ok(Some(delivery)) => {
+                let content = delivery.data.clone();
+                let reader = Reader::new(BufReader::new(&content[..])).unwrap();
+                for record in reader {
+                    let record = record.unwrap();
+                    // we can now process the alert
+                    process_record(record).await;
+                }
+                delivery
+            }
+            // The consumer got canceled
+            Ok(None) => return,
+            // Carries the error and is always followed by Ok(None)
+            Err(error) => {
+                dbg!("Failed to consume queue message {}", error);
+                return;
+            }
+        };
+
+        // Do something with the delivery data (The message payload)
+
+        delivery
+            .ack(BasicAckOptions::default())
+            .await
+            .expect("Failed to ack send_webhook_event message");
+    });
+
+    loop {
+        tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+    }
 }
