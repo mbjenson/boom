89b468da0ccbf46d38e8e65d4a0329bdc517a6de
mbjenson <matthew.jenson@protonmail.com>
2024-08-08 15:24:28 -0500

    moved Epsilon to testing_utils.rs

--- tests/test_unit_utils.rs
+++ tests/test_unit_utils.rs
@@ -5,7 +5,8 @@ use config::{
     Environment,
     File,
 };
-const EPS: f64 = 0.00000005;
+mod testing_utils;
+use testing_utils as tu;
 use boom::structs::CrossmatchConfig;
 use float_cmp::approx_eq;
 
@@ -16,15 +17,15 @@ mod tests {
     #[test]
     fn test_utils_great_circle_distance() {
         let x = utils::great_circle_distance(10.0, 12.0, 5.2, 8.5);
-        assert!(approx_eq!(f64, x, 5.878131520996996, epsilon = EPS));
+        assert!(approx_eq!(f64, x, 5.878131520996996, epsilon = tu::EPS));
     }
 
     #[test]
     fn test_utils_radec2lb() {
         let x = utils::radec2lb(5.67, 35.2);
         let answer = (116.32324022484094, -27.302450971293737);
-        assert!(approx_eq!(f64, x.0, answer.0, epsilon = EPS));
-        assert!(approx_eq!(f64, x.1, answer.1, epsilon = EPS));
+        assert!(approx_eq!(f64, x.0, answer.0, epsilon = tu::EPS));
+        assert!(approx_eq!(f64, x.1, answer.1, epsilon = tu::EPS));
     }
 
     #[test]
@@ -63,12 +64,12 @@ mod tests {
             distance_max_near: 300.0, // 5 arcsec for objects that are too close (z < 0.01)
             distance_unit: "redshift".to_string(),
         };
-        assert!(approx_eq!(f64, clu_config.radius, _clu_config.radius, epsilon = EPS));
+        assert!(approx_eq!(f64, clu_config.radius, _clu_config.radius, epsilon = tu::EPS));
         assert_eq!(clu_config.use_distance, _clu_config.use_distance);
         assert_eq!(clu_config.distance_key, _clu_config.distance_key);
         assert_eq!(clu_config.distance_unit, _clu_config.distance_unit);
-        assert!(approx_eq!(f64, clu_config.distance_max, _clu_config.distance_max, epsilon = EPS));
-        assert!(approx_eq!(f64, clu_config.distance_max_near, _clu_config.distance_max_near, epsilon = EPS));
+        assert!(approx_eq!(f64, clu_config.distance_max, _clu_config.distance_max, epsilon = tu::EPS));
+        assert!(approx_eq!(f64, clu_config.distance_max_near, _clu_config.distance_max_near, epsilon = tu::EPS));
     }
 
     #[test]
@@ -87,12 +88,12 @@ mod tests {
             distance_max_near: 300.0, // 5 arcsec for objects that are too close (z < 0.01)
             distance_unit: "Mpc".to_string(),
         };
-        assert!(approx_eq!(f64, ned_config.radius, _ned_config.radius, epsilon = EPS));
+        assert!(approx_eq!(f64, ned_config.radius, _ned_config.radius, epsilon = tu::EPS));
         assert_eq!(ned_config.use_distance, _ned_config.use_distance);
         assert_eq!(ned_config.distance_key, _ned_config.distance_key);
         assert_eq!(ned_config.distance_unit, _ned_config.distance_unit);
-        assert!(approx_eq!(f64, ned_config.distance_max, _ned_config.distance_max, epsilon = EPS));
-        assert!(approx_eq!(f64, ned_config.distance_max_near, _ned_config.distance_max_near, epsilon = EPS));
+        assert!(approx_eq!(f64, ned_config.distance_max, _ned_config.distance_max, epsilon = tu::EPS));
+        assert!(approx_eq!(f64, ned_config.distance_max_near, _ned_config.distance_max_near, epsilon = tu::EPS));
     }
 
     #[test]
@@ -108,12 +109,12 @@ mod tests {
             use_distance: false,
             ..Default::default()
         };
-        assert!(approx_eq!(f64, milliquas_config.radius, _milliquas_config.radius, epsilon = EPS));
+        assert!(approx_eq!(f64, milliquas_config.radius, _milliquas_config.radius, epsilon = tu::EPS));
         assert_eq!(milliquas_config.use_distance, _milliquas_config.use_distance);
         assert_eq!(milliquas_config.distance_key, _milliquas_config.distance_key);
         assert_eq!(milliquas_config.distance_unit, _milliquas_config.distance_unit);
-        assert!(approx_eq!(f64, milliquas_config.distance_max, _milliquas_config.distance_max, epsilon = EPS));
-        assert!(approx_eq!(f64, milliquas_config.distance_max_near, _milliquas_config.distance_max_near, epsilon = EPS));
+        assert!(approx_eq!(f64, milliquas_config.distance_max, _milliquas_config.distance_max, epsilon = tu::EPS));
+        assert!(approx_eq!(f64, milliquas_config.distance_max_near, _milliquas_config.distance_max_near, epsilon = tu::EPS));
     }
 
 }
