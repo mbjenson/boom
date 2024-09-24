1521a1d9f5107afb74d8233911535b085dbe88d2
mbjenson <matthew.jenson@protonmail.com>
2024-09-24 14:12:50 -0500

    added Sig Interrupt handling

--- src/bin/filter_worker.rs
+++ src/bin/filter_worker.rs
@@ -2,11 +2,11 @@ use redis::AsyncCommands;
 use redis::streams::{StreamReadOptions,StreamReadReply};
 use std::{
     env, 
-    error::Error, 
+    error::Error,
+    sync::{Arc, Mutex},
 };
 
 use boom::{conf, filter};
-
 
 async fn get_candids_from_stream(con: &mut redis::aio::MultiplexedConnection, stream: &str, options: &StreamReadOptions) -> Vec<i64> {
     let result: Option<StreamReadReply> = con.xread_options(
@@ -35,6 +35,16 @@ async fn get_candids_from_stream(con: &mut redis::aio::MultiplexedConnection, st
     candids
 }
 
+// intercepts interrupt signals and sets boolean to true
+async fn sig_int_handler(v: Arc<Mutex<bool>>) {
+    tokio::spawn(async move {
+        tokio::signal::ctrl_c().await.unwrap();
+        print!("got sigint");
+        let mut v = v.try_lock().unwrap();
+        *v = true;
+    });
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
@@ -43,6 +53,10 @@ async fn main() -> Result<(), Box<dyn Error>> {
         filter_id = args[1].parse::<i32>().unwrap();
     }
 
+    // setup signal handler thread
+    let interrupt = Arc::new(Mutex::new(false));
+    sig_int_handler(Arc::clone(&interrupt)).await;
+
     // connect to mongo and redis
     let config_file = conf::load_config("./config.yaml").unwrap();
     let db = conf::build_db(&config_file, true).await;
@@ -50,8 +64,6 @@ async fn main() -> Result<(), Box<dyn Error>> {
         "redis://localhost:6379".to_string()).unwrap();
     let mut con = client_redis
         .get_multiplexed_async_connection().await.unwrap();
-    
-    // (OLD) let mut filter = filter::Filter::build(filter_id, &db).await?;
 
     // build filter
     let mut filter = match filter::Filter::build(filter_id, &db).await {
@@ -91,14 +103,22 @@ async fn main() -> Result<(), Box<dyn Error>> {
     }
 
     let opts = StreamReadOptions::default()
-    .group(&consumer_group, "worker_1")
-    .count(100);
+        .group(&consumer_group, "worker_1")
+        .count(100);
 
     loop {
-        let candids = get_candids_from_stream(&mut con, &redis_stream, &opts).await;
+        // check if worker has been interrupted
+        let should_exit: bool = match interrupt.try_lock() {
+            Ok(x) => { *x },
+            Err(_) => { false }
+        };
 
+        let candids = get_candids_from_stream(&mut con, &redis_stream, &opts).await;
         if candids.len() == 0 {
             println!("Queue empty");
+            if should_exit {
+                return Ok(())
+            }
             tokio::time::sleep(tokio::time::Duration::from_secs(3)).await;
             continue;
         }
@@ -122,5 +142,9 @@ async fn main() -> Result<(), Box<dyn Error>> {
         con.lpush::<&str, &Vec<String>, isize>(&filter_results_queue, &out_documents).await.unwrap();
 
         println!("{}/{} alerts passed filter {} in {}s", out_documents.len(), in_count, filter_id, start.elapsed().as_secs_f64());
+
+        if should_exit {
+            return Ok(())
+        }
     }
 }
