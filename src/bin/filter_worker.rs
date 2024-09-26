53e338c48af911216aff529f287b31d34cdb50e2
mbjenson <matthew.jenson@protonmail.com>
2024-09-26 14:25:14 -0500

    implemented a rough sketch version of a filter worker which takes and operates on multiple filters

--- src/bin/filter_worker.rs
+++ src/bin/filter_worker.rs
@@ -4,6 +4,7 @@ use std::{
     env, 
     error::Error,
     sync::{Arc, Mutex},
+    collections::HashMap,
 };
 
 use boom::{conf, filter};
@@ -39,19 +40,33 @@ async fn get_candids_from_stream(con: &mut redis::aio::MultiplexedConnection, st
 async fn sig_int_handler(v: Arc<Mutex<bool>>) {
     tokio::spawn(async move {
         tokio::signal::ctrl_c().await.unwrap();
-        print!("got sigint");
+        print!("Filter worker interrupted. Finishing up...");
         let mut v = v.try_lock().unwrap();
         *v = true;
     });
 }
 
+/*
+    TODO: figure out how the streams work with permissions
+        i.e. does the stream for permission level 3 contain everything in and under permission level 3
+             or, does a stream for permission level 3 contain just the data which is specifically made
+             for permission level 3.
+*/
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
-    let mut filter_id = 1;
-    if args.len() > 1 {
-        filter_id = args[1].parse::<i32>().unwrap();
+    // let mut filter_id = 1;
+    // get all the filter_ids
+    let mut filter_ids: Vec<i32> = Vec::new();
+    for i in 1..args.len() {
+        filter_ids.push(args[i].parse::<i32>().unwrap());
     }
+    println!("filter ids: {:?}", filter_ids);
+    // if args.len() > 1 {
+    //     for i in 0..args.len()
+    //     filter_id = args[1].parse::<i32>().unwrap();
+    // }
 
     // setup signal handler thread
     let interrupt = Arc::new(Mutex::new(false));
@@ -65,46 +80,106 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let mut con = client_redis
         .get_multiplexed_async_connection().await.unwrap();
 
-    // build filter
-    let mut filter = match filter::Filter::build(filter_id, &db).await {
-        Ok(filter) => {
-            filter
-        },
-        Err(e) => {
-            println!("got error: {}", e);
-            return Err(e);
+    // build filter(s)
+    let mut filters: Vec<filter::Filter> = Vec::new();
+    for id in filter_ids.clone() {
+        let filter = filter::Filter::build(id, &db).await;
+        match filter {
+            Ok(filter) => {
+                filters.push(filter);
+            },
+            Err(e) => {
+                println!("got error when trying to build filter {}: {}", id, e);
+                return Err(e);
+            }
         }
-    };
+    }
+    println!("filters.len(): {}", filters.len());
 
-    println!("Starting filter worker for filter {}", filter_id);
+    // build filter
+    // let mut filter = match filter::Filter::build(filter_id, &db).await {
+    //     Ok(filter) => {
+    //         filter
+    //     },
+    //     Err(e) => {
+    //         println!("got error: {}", e);
+    //         return Err(e);
+    //     }
+    // };
+    
+    println!("Starting filter worker for filters {:?}", filter_ids);
+    // println!("Starting filter worker for filter {}", filter_id);
 
-    let redis_stream = format!("{stream}_programid_{programid}_filter_stream", 
-        stream=filter.catalog, programid=filter.permissions.iter().max().unwrap());
+    let mut redis_streams = HashMap::new();
+    for filter in &filters {
+        let stream = format!("{stream}_programid_{programid}_filter_stream",
+            stream = filter.catalog, programid = filter.permissions.iter().max().unwrap());
+        if !redis_streams.contains_key(filter.permissions.iter().max().unwrap()) {
+            redis_streams.insert(filter.permissions.iter().max().unwrap().clone(), stream);
+        }
+    }
+    println!("redis streams: {:?}", redis_streams);
+    
+    // let redis_stream = format!("{stream}_programid_{programid}_filter_stream", 
+    //     stream=filter.catalog, programid=filter.permissions.iter().max().unwrap());
 
     // this uses consumer groups to more easily "resume" reading from where we might have stopped
     // as well as to allow multiple workers to read from the same stream for a given filter,
     // which let's us scale the number of workers per filter if needed in the future
     // https://medium.com/redis-with-raphael-de-lio/understanding-redis-streams-33aa96ca7206
+    
+    let mut filter_results_queues = HashMap::new();
 
-    // let consumer_group = format!("filter_{filter_id}_{rand}", filter_id=filter_id, rand=rand::random::<i32>());
-    let consumer_group = format!("filter_{filter_id}_group", filter_id=filter_id);
-    let consumer_group_res: Result<(), redis::RedisError> = con.xgroup_create(
-        &redis_stream, &consumer_group, "0").await;
-
-    let filter_results_queue = format!("filter_{filter_id}_results", filter_id=filter_id);
-
-    match consumer_group_res {
-        Ok(_) => {
-            println!("Created consumer group for filter {}", filter_id);
-        },
-        Err(e) => {
-            println!("Consumer group already exists for filter {}: {:?}", filter_id, e);
+    let mut consumer_groups = HashMap::new();
+    for filter in &filters {
+        let consumer_group = format!("filter_{filter_id}_group", filter_id = filter.id);
+        let consumer_group_res: Result<(), redis::RedisError> = con.xgroup_create(
+            &redis_streams[filter.permissions.iter().max().unwrap()], &consumer_group, "0").await;
+        match consumer_group_res {
+            Ok(()) => {
+                println!("Created consumer group for filter {}", filter.id);
+            },
+            Err(e) => {
+                println!("Consumer group already exists for filter {}: {:?}", filter.id, e);
+            }
         }
+        consumer_groups.insert(filter.id, consumer_group);
+        filter_results_queues.insert(filter.id, format!("filter_{filter_id}_results", filter_id = filter.id));
     }
 
-    let opts = StreamReadOptions::default()
-        .group(&consumer_group, "worker_1")
-        .count(100);
+    println!("consumer groups: {:?}", consumer_groups);
+    println!("filter_results_queues: {:?}", filter_results_queues);
+
+    // let consumer_group = format!("filter_{filter_id}_group", filter_id=filter_id);
+    // let consumer_group_res: Result<(), redis::RedisError> = con.xgroup_create(
+    //     &redis_stream, &consumer_group, "0").await;
+
+    // let filter_results_queue = format!("filter_{filter_id}_results", filter_id=filter_id);
+
+    // match consumer_group_res {
+    //     Ok(_) => {
+    //         println!("Created consumer group for filter {}", filter_id);
+    //     },
+    //     Err(e) => {
+    //         println!("Consumer group already exists for filter {}: {:?}", filter_id, e);
+    //     }
+    // }
+
+    let mut read_options = HashMap::new();
+    for filter_id in consumer_groups.keys() {
+        let opts = StreamReadOptions::default()
+            .group(&consumer_groups[filter_id], "worker_1")
+            .count(100);
+        read_options.insert(filter_id, opts);
+    }
+
+    println!("read options: {:?}", read_options);
+    // let opts = StreamReadOptions::default()
+    //     .group(&consumer_group, "worker_1")
+    //     .count(100);
+
+    // keep track of how many streams are empty in order to take breaks
+    let mut empty_stream_counter: usize = 0;
 
     loop {
         // check if worker has been interrupted
@@ -115,33 +190,53 @@ async fn main() -> Result<(), Box<dyn Error>> {
             _ => {}
         };
 
-        let candids = get_candids_from_stream(&mut con, &redis_stream, &opts).await;
-        if candids.len() == 0 {
-            println!("Queue empty");
+        for filter in &mut filters {
+            let candids = get_candids_from_stream(
+                &mut con,
+                &redis_streams[filter.permissions.iter().max().unwrap()], 
+                &read_options[&filter.id]).await;
+            if candids.len() == 0 {
+                println!("Stream empty");
+                // tokio::time::sleep(tokio::time::Duration::from_secs(3)).await;
+                empty_stream_counter += 1;
+                continue;
+            }
+
+            let in_count = candids.len();
+
+            println!("got {} candids from a redis stream for filter {}", in_count, redis_streams[filter.permissions.iter().max().unwrap()]);
+            println!("running filter with id {} on {} alerts", filter.id, in_count);
+
+            let start = std::time::Instant::now();
+
+            let out_documents = filter.run(candids, &db).await.unwrap();
+            if out_documents.len() == 0 {
+                continue;
+            }
+
+            // convert the documents to a format that any other worker (even in python) can read
+            // for that we can deserialize the Document to json
+            let out_documents: Vec<String> = out_documents.iter().map(|doc| {
+                let json = serde_json::to_string(doc).unwrap();
+                json
+            }).collect();
+            con.lpush::<&str, &Vec<String>, isize>(
+                &filter_results_queues[&filter.id],
+                 &out_documents).await.unwrap();
+
+            println!(
+                "{}/{} alerts passed filter {} in {}s", 
+                out_documents.len(), in_count, filter.id, start.elapsed().as_secs_f64());
+
+            println!("finished with filter {}", filter.id);
             tokio::time::sleep(tokio::time::Duration::from_secs(3)).await;
-            continue;
+            
         }
-
-        let in_count = candids.len();
-        println!("running filter with id {} on {} alerts", filter_id, in_count);
-
-        let start = std::time::Instant::now();
-
-        let out_documents = filter.run(candids, &db).await.unwrap();
-        if out_documents.len() == 0 {
-            continue;
+        if empty_stream_counter == filters.len() {
+            println!("All streams empty");
+            tokio::time::sleep(tokio::time::Duration::from_secs(3)).await;
         }
-
-        // convert the documents to a format that any other worker (even in python) can read
-        // for that we can deserialize the Document to json
-        let out_documents: Vec<String> = out_documents.iter().map(|doc| {
-            let json = serde_json::to_string(doc).unwrap();
-            json
-        }).collect();
-        con.lpush::<&str, &Vec<String>, isize>(&filter_results_queue, &out_documents).await.unwrap();
-
-        println!(
-            "{}/{} alerts passed filter {} in {}s", 
-            out_documents.len(), in_count, filter_id, start.elapsed().as_secs_f64());
+        empty_stream_counter = 0;
     }
+    
 }
