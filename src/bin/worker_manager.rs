616644cb8c65d1d55cebcc07c0853ae36be30384
mbjenson <matthew.jenson@protonmail.com>
2024-10-17 15:10:32 -0500

    added automatic worker interruption using the nix library

--- src/bin/worker_manager.rs
+++ src/bin/worker_manager.rs
@@ -1,4 +1,4 @@
-use std::process;
+use std::{borrow::Borrow, process};
 use tokio::time;
 
 use redis::AsyncCommands;
@@ -11,6 +11,8 @@ use std::{
 use nix;
 
 use boom::worker_util;
+
+use std::io;
 
 /*
 > multi-worker-manager
@@ -42,45 +44,48 @@ outputted data streams could be queried for data throughput metrics somehow.
 1. add a place to display how many workers are currently active and of which types
 
 */
-
-
 // total_outgoing += con.llen::<&str, isize>("ZTF_alerts_classifier_queue").await.unwrap() as i64;
 
 
-// send 
-async fn exit_workers(workers: &mut HashMap<(), std::process::Child>) -> Result<(), Box<dyn Error>> {
-    for worker in workers.values_mut() {
-        worker.kill().expect("worker could not be killed")
+fn interrupt_worker(worker: &std::process::Child) -> Result<(), nix::errno::Errno> {
+    match nix::sys::signal::kill(
+        nix::unistd::Pid::from_raw(worker.id() as i32),
+        nix::sys::signal::Signal::SIGINT
+    ) {
+        Err(e) => { return Err(e) },
+        Ok(_) => { Ok(()) }
     }
-    Ok(())
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let interrupt = Arc::new(Mutex::new(false));
     worker_util::sig_int_handler(Arc::clone(&interrupt), "manager".to_string()).await;
+
     // REDIS
-    let client_redis = redis::Client::open(
-        "redis://localhost:6379".to_string()
-    ).unwrap();
-    let mut con = client_redis.get_multiplexed_async_connection().await.unwrap();
+    // let client_redis = redis::Client::open(
+    //     "redis://localhost:6379".to_string()
+    // ).unwrap();
+    // let mut con = client_redis.get_multiplexed_async_connection().await.unwrap();
 
     let mut worker_table: HashMap<&str, Vec<process::Child>> = HashMap::from([
         ("alert", Vec::new()),
         ("ml", Vec::new()),
         ("filter", Vec::new())
     ]);
-    // let alert_worker = process::Command::new("./target/debug/alert_worker")
-    //     .arg("ZTF")
-    //     .spawn()
-    //     .expect("failed to start alert worker");
-    // worker_table.entry("alert").and_modify(|workers| workers.push(alert_worker));
-    
-    // let ml_worker = process::Command::new("python ./py_workers/ml_worker.py")
-    //     .spawn()
-    //     .expect("failed to start ml_worker");
-    // worker_table.entry("ml").and_modify(|workers| workers.push(ml_worker));
 
+    let alert_worker = process::Command::new("./target/debug/alert_worker")
+        .arg("ZTF")
+        .spawn()
+        .expect("failed to start alert worker");
+    worker_table.entry("alert").and_modify(|workers| workers.push(alert_worker));
+    
+    let ml_worker = process::Command::new("python")
+        .arg("./py_workers/ml_worker.py")
+        .spawn()
+        .expect("failed to start ml_worker");
+    worker_table.entry("ml").and_modify(|workers| workers.push(ml_worker));
+    
     let filter_worker = process::Command::new("./target/debug/filter_worker")
         .args(["2", "3", "4"])
         .spawn()
@@ -88,28 +93,19 @@ async fn main() -> Result<(), Box<dyn Error>> {
     worker_table.entry("filter").and_modify(|workers| workers.push(filter_worker));
     
     loop {
-        // match interrupt.try_lock() {
-        //     Ok(flag) => {
-        //         if *flag {
-        //             exit_children()
-        //         }
-        //     },
-        //     _ => {}
-        // }
-
-        
-        
+        // check if manager has received SIGINT
         match interrupt.try_lock() {
             Ok(stop) => {
                 if *stop {
                     for (_, worker_vec) in worker_table {
-                        
                         for worker in worker_vec {
-                            nix::sys::signal::kill(
-                                nix::unistd::Pid::from_raw(worker.id() as i32),
-                                nix::sys::signal::Signal::SIGINT
-                            ).expect("cannot send ctrl-c");
-                            // worker.kill().unwrap();
+                            let res = interrupt_worker(&worker);
+                            match res {
+                                Err(e) => {
+                                    println!("could not interrupt worker {}, got errno: {:?}", &worker.id(), e);
+                                },
+                                Ok(_) => {}
+                            }
                         }
                     }
                     return Ok(());
@@ -117,7 +113,13 @@ async fn main() -> Result<(), Box<dyn Error>> {
             },
             _ => {}
         };
+        
+        // get a tally of how many workers are running and print it out
+        println!("==== WORKERS ====");
+        for (worker_type, worker_vec) in worker_table.borrow() {
+            println!("type: {}    amount: {}", worker_type, worker_vec.len());
+        }
+
         tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
-        // start one of each worker as a child process
     }
 }
