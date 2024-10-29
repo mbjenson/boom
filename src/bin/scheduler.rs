208ba616fe3c81eb29ad1f5a88f1fb9496392baa
mbjenson <matthew.jenson@protonmail.com>
2024-10-29 12:10:23 -0500

    added filter worker function into scheduler for testing

--- src/bin/scheduler.rs
+++ src/bin/scheduler.rs
@@ -1,16 +1,160 @@
 use std::{
-    borrow::Borrow, collections::HashMap, fmt, sync::{mpsc, Arc, Mutex}, thread::{self, JoinHandle}, error::Error,
+    collections::HashMap, error::Error, fmt, string, sync::{mpsc, Arc, Mutex}, thread::{self, JoinHandle}
 };
 use futures::executor;
 use futures;
 use boom::{types::Alert, worker_util};
 use rdkafka::message::Header;
 use tokio::{self, sync::{broadcast::error, watch::Receiver}};
-use redis::AsyncCommands;
+use redis::{AsyncCommands, streams::StreamReadOptions};
 use std::env;
-use boom::{conf, alert, types::ztf_alert_schema};
+use boom::{filter, conf, alert, types::ztf_alert_schema};
+
+// TODO: implement fake ml_worker which takes in alerts, opens streams, and sends candids 
+//       to streams without running any ml.
 
 
+#[tokio::main]
+async fn filter_worker(id: String, receiver: Arc<Mutex<mpsc::Receiver<WorkerCmd>>>) -> Result<(), Box<dyn Error>> {
+    let catalog = "ZTF";
+
+    let filters = vec![3];
+    let mut filter_ids: Vec<i32> = Vec::new();
+
+    for i in 0..filters.len() {
+        if !filter_ids.contains(&filters[i]) {
+            filter_ids.push(filters[i]);
+        }
+    }
+    println!("Starting filter worker for {} with filters {:?}", catalog, filter_ids);
+
+    // connect to mongo and redis
+    let config_file = conf::load_config("config.yaml").unwrap();
+    let db = conf::build_db(&config_file).await;
+    let client_redis = redis::Client::open(
+        "redis://localhost:6379".to_string()).unwrap();
+    let mut con = client_redis
+        .get_multiplexed_async_connection().await.unwrap();
+    
+    // build filters and organize by permission level
+    let mut filter_table: HashMap<i64, Vec<filter::Filter>> = HashMap::new();
+    for id in filter_ids.clone() {
+        let filter = filter::Filter::build(id, &db).await;
+        match filter {
+            Ok(filter) => {
+                let perms = filter.permissions.iter().max().unwrap();
+                if !filter_table.contains_key(perms) {
+                    filter_table.insert(perms.clone(), Vec::new());
+                }
+                filter_table.entry(perms.clone()).and_modify(|filters| filters.push(filter));
+            },
+            Err(e) => {
+                println!("got error when trying to build filter {}: {}", id, e);
+                return Err(e);
+            }
+        }
+    }
+
+    // initialize redis streams based on permission levels
+    let mut redis_streams = HashMap::new();
+    for filter_vec in filter_table.values() {
+        for filter in filter_vec {
+            let perms = filter.permissions.iter().max().unwrap();
+            let stream = format!("{stream}_programid_{programid}_filter_stream",
+                stream = filter.catalog, programid = perms);
+            if !redis_streams.contains_key(perms) {
+                redis_streams.insert(perms.clone(), stream);
+            }
+        }
+    }
+
+    // create consumer groups, read_options, and output queues for each filter
+    let mut read_options = HashMap::new();
+    let mut filter_results_queues: HashMap<i32, String> = HashMap::new();
+    for filter_vec in filter_table.values() {
+        for filter in filter_vec {
+            let consumer_group = format!("filter_{filter_id}_group", filter_id = filter.id);
+            let consumer_group_res: Result<(), redis::RedisError> = con.xgroup_create(
+                &redis_streams[filter.permissions.iter().max().unwrap()], 
+                &consumer_group, "0").await;
+            match consumer_group_res {
+                Ok(()) => {
+                    println!("Created consumer group for filter {}", filter.id);
+                },
+                Err(e) => {
+                    println!("Consumer group already exists for filter {}: {:?}", filter.id, e);
+                }
+            }
+            let opts = StreamReadOptions::default()
+                .group(consumer_group, "worker_1")
+                .count(100);
+            read_options.insert(filter.id, opts);
+            filter_results_queues.insert(
+                filter.id, 
+                format!("filter_{filter_id}_results", filter_id = filter.id));
+        }
+    }
+
+    // keep track of how many streams are empty in order to take breaks
+    let mut empty_stream_counter: usize = 0;
+
+    loop {
+        // check for command from threadpool
+        if let Ok(command) = receiver.lock().unwrap().try_recv() {
+            match command {
+                WorkerCmd::TERM => {
+                    println!("alert worker {} received termination command", id);
+                    return Ok(());
+                },
+            }
+        }
+
+        for (perm, filters) in &mut filter_table {
+            for filter in filters {
+                let candids = worker_util::get_candids_from_stream(
+                    &mut con,
+                    &redis_streams[&perm],
+                    &read_options[&filter.id]).await;
+                if candids.len() == 0 {
+                    empty_stream_counter += 1;
+                    continue;
+                }
+                let in_count = candids.len();
+
+                println!("got {} candids from redis stream for filter {}", in_count, redis_streams[&perm]);
+                println!("running filter with id {} on {} alerts", filter.id, in_count);
+
+                let start = std::time::Instant::now();
+
+                let out_documents = filter.run(candids, &db).await.unwrap();
+                if out_documents.len() == 0 {
+                    continue;
+                }
+                // convert the documents to a format that any other worker (even in python) can read
+                // for that we can deserialize the Document to json
+                let out_documents: Vec<String> = out_documents.iter().map(|doc| {
+                    let json = serde_json::to_string(doc).unwrap();
+                    json
+                }).collect();
+                con.lpush::<&str, &Vec<String>, isize>(
+                    &filter_results_queues[&filter.id],
+                    &out_documents).await.unwrap();
+
+                println!(
+                    "{}/{} alerts passed filter {} in {}s", 
+                    out_documents.len(), in_count, filter.id, start.elapsed().as_secs_f64());
+            }
+        }
+        if empty_stream_counter == filter_ids.len() {
+            println!("All streams empty");
+            tokio::time::sleep(tokio::time::Duration::from_secs(3)).await;
+        }
+        empty_stream_counter = 0;
+    }
+}
+
+
+// TODO: include original env::args as func args
 // alert worker as a standalone function which is run by the scheduler
 #[tokio::main]
 async fn alert_worker(id: String, receiver: Arc<Mutex<mpsc::Receiver<WorkerCmd>>>) {
@@ -249,7 +393,7 @@ impl Drop for ThreadPool {
         }
     }
 }
-    
+
 
 /*
 Worker Struct
@@ -275,6 +419,11 @@ impl Worker {
                     alert_worker(id, receiver);
                 })
             },
+            WorkerType::Filter => {
+                thread::spawn(|| {
+                    filter_worker(id, receiver);
+                })
+            },
             _ => {
                 panic!("worker type not yet implemnted");
             }
@@ -286,7 +435,6 @@ impl Worker {
         }
     }
 }
-
 
 
 // some pseudocode
@@ -318,8 +466,10 @@ async fn main() {
     let interrupt = Arc::new(Mutex::new(false));
     worker_util::sig_int_handler(Arc::clone(&interrupt)).await;
     
-    let alert_pool = ThreadPool::new(WorkerType::Alert, 3);
-    
+    // let alert_pool = ThreadPool::new(WorkerType::Alert, 3);
+    let filter_pool = ThreadPool::new(WorkerType::Filter, 3);
+    // let ml_pool = ThreadPool::new(WorkerType::ML, 3);
+
     // println!("spawned in 1 worker of each type");
     loop {
         let exit = worker_util::check_flag(Arc::clone(&interrupt));
@@ -329,7 +479,10 @@ async fn main() {
         thread::sleep(std::time::Duration::from_secs(1));
         if exit {
             println!("killed thread(s)");
-            drop(alert_pool);
+            // drop(alert_pool);
+            drop(filter_pool);
+            // drop(ml_pool);
+
             // let _ = v.into_iter().map(|x| x.join());
             break;
         }
@@ -350,6 +503,7 @@ async fn main() {
 
     std::process::exit(0);
 }
+
 
 // some notes about tokio tasks and other things
 
